84b2bdee5fe712bfc67d4e4e09abe39ea028fa4d
Granth Mangukiya <granth0407@gmail.com>
2023-12-07 06:47:31 +0530

    #3_1 & #3_2 over

---
+++ 3_2.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int apples;
+    cout<<"How many apples do you have?"<<endl;
+    cin >> apples;
+    cout << "Number of Apples you have is : " << apples << endl;
+    return 0;
+
+}
